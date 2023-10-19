d85b7ba9aa79ce8eeb22b86f69ba6bce968a84db
EleazarF <eleazar.fuentes@gmail.com>
2023-10-19 17:47:32 -0600

    Update htmlRoutes.js
    including module imports, the exported function, and the route setup.

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,10 @@
+// Import the 'path' module to work with file paths
 const path = require('path');
 
+// Export a function that sets up a route for the root URL ("/") using the 'app' object
 module.exports = (app) =>
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  // Handle HTTP GET requests to the root URL
+  app.get("/", (req, res) =>
+    // Send the HTML file located at the specified path
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
   );
