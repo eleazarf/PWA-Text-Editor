6ff8d5bb183b467100b833d6ad6a591263246077
EleazarF <eleazar.fuentes@gmail.com>
2023-10-19 17:44:50 -0600

    Update src-sw.js
    Including the import of Workbox modules, the configuration of caching strategies, and the registration of service worker routes.

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,50 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// Import necessary Workbox modules for service worker strategies and caching
+const {
+  offlineFallback,
+  warmStrategyCache,
+} = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
+// Precache resources specified in the '__WB_MANIFEST' array
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Define a cache strategy for pages
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache pages for 30 days
     }),
   ],
 });
 
+// Warm the cache for specific URLs using the 'pageCache' strategy
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Register a route for page navigation requests using the 'pageCache' strategy
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implement asset caching strategy
+registerRoute(
+  ({ request }) =>
+    ["style", "script", "worker"].includes(request.destination), // Filter asset requests
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache", // Define the name of the asset cache
+    plugins: [
+      // Cache responses with these statuses up to a maximum of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
