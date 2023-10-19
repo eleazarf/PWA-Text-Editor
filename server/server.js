b1f141cb495ac2154327beddf56e64716ceaa22d
EleazarF <eleazar.fuentes@gmail.com>
2023-10-19 17:48:43 -0600

    Update server.js
    including module imports, middleware setup, route initialization, and starting the server.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
-const express = require('express');
+// Import the 'express' framework
+const express = require("express");
 
+// Create an instance of the 'express' application
 const app = express();
+
+// Define the port for the server, using the environment variable 'PORT' or defaulting to 3000
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Middleware Setup
+
+// Serve static files from the '../client/dist' directory
+app.use(express.static("../client/dist"));
+
+// Parse URL-encoded and JSON request bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+// Require and initialize HTML routes for the application
+require("./routes/htmlRoutes")(app);
 
+// Start the server and listen on the specified port
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
