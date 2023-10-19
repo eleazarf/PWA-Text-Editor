5fa21548c062571eafd06a8def0acc4a06250d1b
EleazarF <eleazar.fuentes@gmail.com>
2023-10-19 17:41:06 -0600

    Update index.js
    Including module imports, component instantiation, and service worker registration.

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,33 +1,39 @@
-import { Workbox } from 'workbox-window';
-import Editor from './editor';
-import './database';
-import '../css/style.css';
+// Import necessary modules and components
+import { Workbox } from "workbox-window";
+import Editor from "./editor";
+import "./database"; // Import the database module
+import "../css/style.css"; // Import styles
 
-const main = document.querySelector('#main');
-main.innerHTML = '';
+// Find the HTML element with the ID "main" and clear its content
+const main = document.querySelector("#main");
+main.innerHTML = "";
 
+// Function to display a loading spinner
 const loadSpinner = () => {
-  const spinner = document.createElement('div');
-  spinner.classList.add('spinner');
+  const spinner = document.createElement("div");
+  spinner.classList.add("spinner");
   spinner.innerHTML = `
   <div class="loading-container">
-  <div class="loading-spinner" />
+    <div class="loading-spinner" />
   </div>
   `;
   main.appendChild(spinner);
 };
 
+// Create an instance of the 'Editor' class
 const editor = new Editor();
 
-if (typeof editor === 'undefined') {
+// If the 'editor' instance is not defined, display a loading spinner
+if (typeof editor === "undefined") {
   loadSpinner();
 }
 
-// Check if service workers are supported
-if ('serviceWorker' in navigator) {
-  // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+// Check if service workers are supported in the browser
+if ("serviceWorker" in navigator) {
+  // Register a Workbox service worker for offline capabilities
+  const workboxSW = new Workbox("/src-sw.js");
   workboxSW.register();
 } else {
-  console.error('Service workers are not supported in this browser.');
+  // Log an error message if service workers are not supported
+  console.error("Service workers are not supported in this browser.");
 }
