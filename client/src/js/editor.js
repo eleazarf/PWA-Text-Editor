702cfb1b8bf50ef7a479a4507ea0e66f7ad22676
EleazarF <eleazar.fuentes@gmail.com>
2023-10-19 17:30:46 -0600

    Update editor.js
    code and comments to the class constructor and its interactions with the CodeMirror editor and data storage.

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,20 +1,25 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, putDb } from './database';
-import { header } from './header';
+// Import methods from './database.js' for saving and retrieving data from IndexedDB
+import { getDb, putDb } from "./database";
 
+// Import the 'header' module
+import { header } from "./header";
+
+// Define a class for a text editor
 export default class {
   constructor() {
-    const localData = localStorage.getItem('content');
+    // Get content from local storage
+    const localData = localStorage.getItem("content");
 
-    // check if CodeMirror is loaded
-    if (typeof CodeMirror === 'undefined') {
-      throw new Error('CodeMirror is not loaded');
+    // Check if CodeMirror library is loaded
+    if (typeof CodeMirror === "undefined") {
+      throw new Error("CodeMirror is not loaded");
     }
 
-    this.editor = CodeMirror(document.querySelector('#main'), {
-      value: '',
-      mode: 'javascript',
-      theme: 'monokai',
+    // Create a CodeMirror editor instance with specific configurations
+    this.editor = CodeMirror(document.querySelector("#main"), {
+      value: "",
+      mode: "javascript",
+      theme: "monokai",
       lineNumbers: true,
       lineWrapping: true,
       autofocus: true,
@@ -22,21 +27,22 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    // When the editor is ready, set its content to the data stored in IndexedDB
+    // If no data is found in IndexedDB, fall back to local storage, and finally to 'header'
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
+      console.info("Loaded data from IndexedDB, injecting into the editor");
       this.editor.setValue(data || localData || header);
     });
 
-    this.editor.on('change', () => {
-      localStorage.setItem('content', this.editor.getValue());
+    // Listen for changes in the editor's content and store it in local storage
+    this.editor.on("change", () => {
+      localStorage.setItem("content", this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
-    this.editor.on('blur', () => {
-      console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+    // Save the content when the editor loses focus
+    this.editor.on("blur", () => {
+      console.log("The editor has lost focus");
+      putDb(localStorage.getItem("content"));
     });
   }
 }
